4d4544c2551f2a020a3526955ca316ef96ba9cc6
LuDotCom <135827528+LuDotCom@users.noreply.github.com>
2023-06-08 15:18:10 +0200

    Update EmailService.java

--- src/main/java/com/testgame/gseven/model/service/EmailService.java
+++ src/main/java/com/testgame/gseven/model/service/EmailService.java
@@ -19,7 +19,25 @@ public class EmailService {
     public EmailService(JavaMailSender javaMailSender) {
         this.javaMailSender = javaMailSender;
     }
-
+	
+    /*
+     * Metodo che sfrutta la {@code SendMail} per inviare una mail con uno specifico link atto al reset della password.
+     * 
+     * @param email Parametro di tipo {@code String} che indica l'indirizzo email dell'utente
+     * a cui mandare la mail.
+     * 
+     * @param siteURL Parametro di tipo {@code String} che indica l'URL del sito web sul quale collegarsi
+     * per ricevere il servizio.
+     * 
+     * @param URLpath Parametro di tipo {@code String} che specifica il percorso "siteURL/.." al quale fare
+     * riferimento.
+     * 
+     * @param passwordToken Parametro di tipo {@code String} che indica il token randomico per poter verificare
+     * la mail in maniera univoca.
+     * 
+     * @return Il metodo non ritorna nessun parametro.
+     * 
+     */
 	public void sendEmailResetPassword(String email, String siteURL,String URLpath ,String passwordToken) {
 		String subject = "Cambio password";
 		String body = "Clicca sul seguente link per cambiare la tua password: " +
@@ -27,12 +45,55 @@ public class EmailService {
 		sendEmail(email, subject, body);
 	}
 	
+	
+	/*
+	 * Metodo che sfrutta la {@code SendMail} per inviare una mail all'indirizzo di posta elettronica inserito dall'utente per 
+	 * verificare la sua validità.
+	 * 
+	 * @param email Parametro di tipo {@code String} che indica l'indirizzo email dell'utente
+    	 * a cui mandare la mail.
+     	 * 
+     	 * @param siteURL Parametro di tipo {@code String} che indica l'URL del sito web sul quale collegarsi
+    	 * per ricevere il servizio.
+     	 * 
+         * 
+         * @param confirmationToken Parametro di tipo {@code String} che indica il token randomico per poter verificare
+         * la mail in maniera univoca per la sua conferma
+         * 
+         * @return Il metodo non ritorna nessun parametro.
+	 * 
+	 */
+	
 	public void sendVerificationEmail(String email, String siteURL, String confirmationToken){
 		String subject = "Conferma registrazione";
 		String body = "Clicca sul seguente link per confermare la tua registrazione: " +
 				siteURL+ "/confirm/token=" + confirmationToken;
 		sendEmail(email, subject, body);
 	}
+	
+	
+	/* Metodo che viene utilizzato per inviare effettivamente una mail  all'indirizzo di posta elettronica
+	 * passato in ingresso. 
+	 * 
+	 * @param email Parametro di tipo {@code String} che indica l'indirizzo email dell'utente
+     * a cui mandare la mail.
+	 * 
+	 * @param subject Parametro di {@code String} che indica il nome dell'oggetto della mail inviata dal servizio. 
+	 * 
+	 * 
+	 * @param body Parametro di tipo {@code String} che rappresenta il body, ossia il corpo, del messaggio
+	 * inviato tramite posta elettronica. 
+	 * 
+	 * @return Il metodo non ritorna nessun parametro.
+	 * 
+	 * 
+	 * @throws MailParseException
+	 * @throws MailAuthenticationException
+	 * @throws MailSendException
+	 * @throws MailException
+	 * 
+	 * 
+	 */
 	
     private void sendEmail(String to, String subject, String body) throws MailParseException, MailAuthenticationException,
     																	MailSendException, MailException{
