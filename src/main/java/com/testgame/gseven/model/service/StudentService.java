87d6ec0420c01a2a6bedd3aa6da28bb202728d0a
emanueledmunina <129553600+emanueledmunina@users.noreply.github.com>
2023-06-08 14:35:46 +0200

    Aggiunto Javadoc a StudentService

--- src/main/java/com/testgame/gseven/model/service/StudentService.java
+++ src/main/java/com/testgame/gseven/model/service/StudentService.java
@@ -39,8 +39,22 @@ public class StudentService implements IStudentService {
 	@Autowired
 	private EmailService emailService;
 	
-	/** Prova javadoc
-	 * @throws StudentAlreadyRegisteredException 
+	/** Metodo che permette di salvare lo studente sulla base dati. Come parametro di ingresso deve
+	 * essere passato un oggetto di tipo {@code Student} con tutti i campi popolati escluso l'{@code id}, che
+	 * verrà creato e associato allo studente automaticamente, operazione che viene svolta dalla base dati.
+	 * Inoltre viene generato un token di conferma dell'identità, che viene inviato tramite email allo studente
+	 * appena registrato al fine di verificare la sua identità.
+	 * L'oggetto {@studentForm} può essere popolato da un apposito form.
+	 * @param studentForm 	oggetto di tipo {@code Student} che deve contenere i campi pieni, al fine di salvare lo
+	 * 						studente sulla base dati
+	 * @return 				Non ritorna valori. Eventuali errori che possono presentarsi,
+	 * 						sono eccezioni che devono che devono essere opportunamente gestite con try-catch.
+	 * @throws StudentAlreadyRegisteredException si sta tentato di salvare nella base dati uno studente
+	 * 											con una email già registrata.
+	 * @throws MailParseException
+	 * @throws MailAuthenticationException
+	 * @throws MailSendException
+	 * @throws MailException
 	 * */
 	@Override
 	public void registerStudent(Student studentForm) throws MailParseException, MailAuthenticationException,
@@ -82,6 +96,16 @@ public class StudentService implements IStudentService {
 		}
 	}
 	
+	/** Metodo che permette di cominciare la procedura di cambio password. In particolare, viene
+	 * generato un token di una pagina univoca, chiamato token password. Il token viene salvato
+	 * sulla base dati e inviata tramite email allo studente.
+	 * @param email parametro di tipo {@code String} che permette di cercare sulla base dati lo studente e inviare il token
+	 * 				da cliccare.
+	 * @eturn		Non ritorna valori. Eventuali errori che possono presentarsi,
+	 * 				sono eccezioni che devono che devono essere opportunamente gestite con try-catch.
+	 * @throws StudentNotFoundException eccezione restituita quando l'email fornita in ingresso	
+	 *									non corrisponde a nessuno studente nella base dati. 
+	 * */
 	@Override
 	public void sendEmailChangePassword(String email, String URLpath) throws StudentNotFoundException {
 		String passwordToken = UUID.randomUUID().toString();
@@ -97,6 +121,21 @@ public class StudentService implements IStudentService {
 		emailService.sendEmailResetPassword(email, "localhost:8080", URLpath ,passwordToken);
 	}
 	
+	/** Metodo che permette di effettuare il salvataggio della nuova password sulla base dati.
+	 * In particolare, viene fornito il  token della password, preso dall'URL della pagina univoca e la
+	 * nuova password in chiaro (NON cifrata). Alla fine del cambio, il token viene cancellato.
+	 * 
+	 * @param passwordToken 	parametro di tipo {@code String} che funge da chiave di ricerca
+	 * 							per selezionare lo studente che ha richiesto il cambio.
+	 * @param newPassword 		parametro di tipo {@code String}, ed è la nuova password scritta
+	 * 							in chiaro (NON cifrata) da salvare nella base dati allo studente
+	 * 							che ha richiesto il cambio.
+	 * @return  				Non ritorna valori. Eventuali errori che possono presentarsi,
+	 * 							sono eccezioni che devono che devono essere opportunamente gestite con try-catch.
+	 * @throws PasswordTokenNotFoundException eccezione restituita quando il token della password fornito in
+	 * 											input non esiste nella base dati		
+	 * 
+	 * */
 	@Override
 	public void changePassword(String passwordToken, String newPassword) throws PasswordTokenNotFoundException {
 		
@@ -111,7 +150,20 @@ public class StudentService implements IStudentService {
 		studentRepository.save(student);
 	}
 	
-
+	/** Metodo che permette di abilitare il nuovo studente registrato. L'abilitazione viene effettuata
+	 * cercando nella base dati il token confirmationToken, di tipo {@code String}, e abilitare
+	 * (cambiare il valore del campi enable da {@code false} a {@code true}) lo studente associato
+	 * a quel token.
+	 * Dal controller, il token lo si può acquisire tramite l'URL o inserito tramite un apposito
+	 * {@code textbox}
+	 * 
+	 * @param confirmationToken	parametro di tipo {@code String} che funge da chiave di ricerca
+	 * 							per selezionare lo studente da abilitare.
+	 * @return  				Non ritorna valori. Eventuali errori che possono presentarsi,
+	 * 							sono eccezioni che devono che devono essere opportunamente gestite con try-catch.
+	 * @throws ConfirmationTokenNotFoundException eccezione restituita quando il token di conferma
+	 * 											 dell'identità non esiste nella base dati. 
+	 * */
 	@Override
 	public void enableStudent(String confirmationToken) throws ConfirmationTokenNotFoundException {
 		boolean isConfirmationTokenFound = findInfoService.doesConfirmationTokenExists(confirmationToken);
